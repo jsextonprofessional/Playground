2d3ac1b2b3fd620559d25bafa6ac4ac8681e998d
jsprofessional <jshuman@protonmail.com>
2021-07-11 20:44:50 -0500

    Routes and templates working

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template  # added render_template!
-app = Flask(__name__)                     
-    
-@app.route('/')                           
-def hello_world():
-    # Instead of returning a string, 
-    # we'll return the result of the render_template method, passing in the name of our HTML file
-    return render_template('index.html')  
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/play/")
+def box_generator_starter():
+    return render_template("index.html", num = 3, color = "lightblue")
+
+@app.route("/play/<int:num>/")
+def box_generator_number(num):
+    return render_template("index.html", num = num, color = "lightblue")
+
+@app.route("/play/<int:num>/<string:color>")
+def box_generator_color(num, color):
+    return render_template("index.html", num = num, color = color)
     
 if __name__=="__main__":
-    app.run(debug=True)                   
-
+    app.run(debug=True)
